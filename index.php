d0061e2b0ce15fd79d942ab1c8a285948ff0c633
eaaaaaaarl <145958305+Kluuuz@users.noreply.github.com>
2023-09-25 13:51:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+echo 'Hello Broasdsafdsgevbhntgmjyfnj nh ';
+
+$value1= 10;
+$value2= 20;
+
+$sum = $value1 + $value2;
+
+echo '<br>';
+echo 'ang pancit canton indi na tag ' .$value1. ' tag ' .$value2. ' na ';
+echo '<br>';
+echo '10 plus 20 is ' .$sum;
+
+
+?>
